3e2a4342d750d847142764e4056e40ad2f309359
alavania007 <al.281194@gmail.com>
2021-08-22 17:58:25 +0530

    Heroku api added

--- src/Utils/Api_Config.js
+++ src/Utils/Api_Config.js
@@ -3,7 +3,7 @@ let hostname = window && window.location && window.location.hostname;
 if (hostname.indexOf("localhost") > -1) {
   hostname = "http://127.0.0.1:5000/api";
 } else {
-  hostname = "http://127.0.0.1:5000/api";
+  hostname = "https://bcg-gamma.herokuapp.com/api";
 }
 
 export default hostname;
